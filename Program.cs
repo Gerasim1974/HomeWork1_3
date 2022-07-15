b83eaf08c95d3a908254adec9a6466167322f9ea
ПК <GIS_1974@tut.by>
2022-07-15 12:50:30 +0300

    HomeWork1 Task3

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number of columns");
+            string? sColumnNumber = Console.ReadLine();
+            int nColumnNumber = int.Parse(sColumnNumber);
+            Console.WriteLine("Please enter distance between columns in meters");
+            string sDistance = Console.ReadLine();
+            int nDistance = int.Parse(sDistance)*100;
+            Console.WriteLine("Please enter the distance between columns in centimeters");
+            string? sWidth = Console.ReadLine();
+            int nWidth = int.Parse(sWidth);
+            int nResult = nDistance*(nColumnNumber-1)+nWidth*(nColumnNumber-2);
+            if (nResult < 0)
+                {
+                nResult = 0;
+                }
+            Console.WriteLine("Result in centimeters = " + Convert.ToString(nResult));
+        }
+    }
+}
